2eb3ae782f73d45c181b91fead657b560c5673f8
Lumthtil <51424300+Lumthtil@users.noreply.github.com>
2019-06-09 17:46:24 -0700

    Update resnet_upsnet.py

--- upsnet/models/resnet_upsnet.py
+++ upsnet/models/resnet_upsnet.py
@@ -93,6 +93,8 @@ class resnet_upsnet(resnet_rcnn):
         channels = data['data'].size()[1]
         edge_filter = [[-0.0751, -0.1238, -0.0751],[-0.1238, 0.7956, -0.1238][-0.0751,-0.1238,-0.0751]]
         edge_filter = torch.FloatTensor(edge_filter).expand(channels,channels,3,3)
+        if torch.cuda.is_avaliable():
+            edge_filter = edge_filter.cuda()
         edge = F.conv2d(data['data'],edge_filter,stride=1,padding=1)
         self.maxpool = nn.MaxPool2d(kernel_size=5, stride=4, padding=2)
         
